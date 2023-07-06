3db06da6ecad85037046aa0645360801127cecb8
openvela-robot <openvela-robot@xiaomi.com>
2023-07-06 16:31:22 +0800

    Fix unused warnings on disable_bounds_checks (#2347)
    Signed-off-by: Huang Qi <huangqi3@xiaomi.com>

--- core/iwasm/compilation/aot_llvm.h
+++ core/iwasm/compilation/aot_llvm.h
@@ -418,7 +418,6 @@ typedef struct AOTCompContext {
     const char *stack_usage_file;
     char stack_usage_temp_file[64];
     const char *llvm_passes;
-    const char *builtin_intrinsics;
 } AOTCompContext;
 
 enum {
@@ -458,7 +457,6 @@ typedef struct AOTCompOption {
     uint32 custom_sections_count;
     const char *stack_usage_file;
     const char *llvm_passes;
-    const char *builtin_intrinsics;
 } AOTCompOption, *aot_comp_option_t;
 
 bool
