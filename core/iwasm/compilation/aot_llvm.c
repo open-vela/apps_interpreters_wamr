576ba875d82fd6cb69c44e39567b40b4dd45a33b
liujinye <liujinye@xiaomi.com>
2023-09-11 17:10:46 +0800

    sync ts2wasm #930
    Signed-off-by: zhangliangyu3 <zhangliangyu3@xiaomi.com>

--- core/iwasm/compilation/aot_llvm.c
+++ core/iwasm/compilation/aot_llvm.c
@@ -146,13 +146,6 @@ aot_target_precheck_can_use_musttail(const AOTCompContext *comp_ctx)
          */
         return false;
     }
-    if (!strcmp(comp_ctx->target_arch, "mips")) {
-        /*
-         * cf.
-         * https://github.com/bytecodealliance/wasm-micro-runtime/issues/2412
-         */
-        return false;
-    }
     /*
      * x86-64/i386: true
      *
@@ -244,10 +237,9 @@ get_inst_extra_offset(AOTCompContext *comp_ctx)
     const AOTCompData *comp_data = comp_ctx->comp_data;
     uint32 table_count = comp_data->import_table_count + comp_data->table_count;
     uint64 offset = get_tbl_inst_offset(comp_ctx, NULL, table_count);
-    uint32 offset_32 = (uint32)offset;
-    bh_assert(offset <= UINT32_MAX);
-    offset_32 = align_uint((uint32)offset_32, 8);
-    return offset_32;
+    bh_assert(offset <= UINT_MAX);
+    offset = align_uint(offset, 8);
+    return offset;
 }
 
 /*
@@ -317,8 +309,8 @@ aot_add_precheck_function(AOTCompContext *comp_ctx, LLVMModuleRef module,
         goto fail;
     }
 
-    uint32 param_count = LLVMCountParams(precheck_func);
-    uint32 sz = param_count * (uint32)sizeof(LLVMValueRef);
+    unsigned int param_count = LLVMCountParams(precheck_func);
+    uint64 sz = param_count * sizeof(LLVMValueRef);
     params = wasm_runtime_malloc(sz);
     if (params == NULL) {
         goto fail;
@@ -634,8 +626,8 @@ aot_add_llvm_func(AOTCompContext *comp_ctx, LLVMModuleRef module,
     if (comp_ctx->is_indirect_mode) {
         /* avoid LUT relocations ("switch-table") */
         LLVMAttributeRef attr_no_jump_tables = LLVMCreateStringAttribute(
-            comp_ctx->context, "no-jump-tables",
-            (uint32)strlen("no-jump-tables"), "true", (uint32)strlen("true"));
+            comp_ctx->context, "no-jump-tables", strlen("no-jump-tables"),
+            "true", strlen("true"));
         LLVMAddAttributeAtIndex(func, LLVMAttributeFunctionIndex,
                                 attr_no_jump_tables);
     }
@@ -903,27 +895,6 @@ create_aux_stack_info(const AOTCompContext *comp_ctx, AOTFuncContext *func_ctx)
 }
 
 static bool
-create_aux_stack_frame(const AOTCompContext *comp_ctx, AOTFuncContext *func_ctx)
-{
-    LLVMValueRef offset = I32_ONE, cur_frame_addr;
-
-    if (!(cur_frame_addr = LLVMBuildInBoundsGEP2(
-              comp_ctx->builder, OPQ_PTR_TYPE, func_ctx->exec_env, &offset, 1,
-              "cur_frame_addr"))) {
-        aot_set_last_error("llvm build in bounds gep failed");
-        return false;
-    }
-
-    if (!(func_ctx->cur_frame = LLVMBuildLoad2(comp_ctx->builder, OPQ_PTR_TYPE,
-                                               cur_frame_addr, "cur_frame"))) {
-        aot_set_last_error("llvm build load failed");
-        return false;
-    }
-
-    return true;
-}
-
-static bool
 create_native_symbol(const AOTCompContext *comp_ctx, AOTFuncContext *func_ctx)
 {
     LLVMValueRef native_symbol_offset = I32_EIGHT, native_symbol_addr;
@@ -957,7 +928,8 @@ create_local_variables(const AOTCompData *comp_data,
                        const AOTCompContext *comp_ctx, AOTFuncContext *func_ctx,
                        const AOTFunc *func)
 {
-    AOTFuncType *aot_func_type = comp_data->func_types[func->func_type_index];
+    AOTFuncType *aot_func_type =
+        (AOTFuncType *)comp_data->types[func->func_type_index];
     char local_name[32];
     uint32 i, j = 1;
 
@@ -1541,7 +1513,8 @@ aot_create_func_context(const AOTCompData *comp_data, AOTCompContext *comp_ctx,
                         AOTFunc *func, uint32 func_index)
 {
     AOTFuncContext *func_ctx;
-    AOTFuncType *aot_func_type = comp_data->func_types[func->func_type_index];
+    AOTFuncType *aot_func_type =
+        (AOTFuncType *)comp_data->types[func->func_type_index];
     WASMModule *module = comp_ctx->comp_data->wasm_module;
     WASMFunction *wasm_func = module->functions[func_index];
     AOTBlock *aot_block;
@@ -1599,11 +1572,6 @@ aot_create_func_context(const AOTCompData *comp_data, AOTCompContext *comp_ctx,
         goto fail;
     }
 
-    if (comp_ctx->enable_aux_stack_frame
-        && !create_aux_stack_frame(comp_ctx, func_ctx)) {
-        goto fail;
-    }
-
     /* Create local variables */
     if (!create_local_variables(comp_data, comp_ctx, func_ctx, func)) {
         goto fail;
@@ -1641,14 +1609,13 @@ aot_create_func_context(const AOTCompData *comp_data, AOTCompContext *comp_ctx,
 fail:
     if (func_ctx->mem_info)
         wasm_runtime_free(func_ctx->mem_info);
-    aot_block_stack_destroy(comp_ctx, &func_ctx->block_stack);
+    aot_block_stack_destroy(&func_ctx->block_stack);
     wasm_runtime_free(func_ctx);
     return NULL;
 }
 
 static void
-aot_destroy_func_contexts(AOTCompContext *comp_ctx, AOTFuncContext **func_ctxes,
-                          uint32 count)
+aot_destroy_func_contexts(AOTFuncContext **func_ctxes, uint32 count)
 {
     uint32 i;
 
@@ -1656,7 +1623,7 @@ aot_destroy_func_contexts(AOTCompContext *comp_ctx, AOTFuncContext **func_ctxes,
         if (func_ctxes[i]) {
             if (func_ctxes[i]->mem_info)
                 wasm_runtime_free(func_ctxes[i]->mem_info);
-            aot_block_stack_destroy(comp_ctx, &func_ctxes[i]->block_stack);
+            aot_block_stack_destroy(&func_ctxes[i]->block_stack);
             aot_checked_addr_list_destroy(func_ctxes[i]);
             wasm_runtime_free(func_ctxes[i]);
         }
@@ -1693,8 +1660,7 @@ aot_create_func_contexts(const AOTCompData *comp_data, AOTCompContext *comp_ctx)
         AOTFunc *func = comp_data->funcs[i];
         if (!(func_ctxes[i] =
                   aot_create_func_context(comp_data, comp_ctx, func, i))) {
-            aot_destroy_func_contexts(comp_ctx, func_ctxes,
-                                      comp_data->func_count);
+            aot_destroy_func_contexts(func_ctxes, comp_data->func_count);
             return NULL;
         }
     }
@@ -1703,7 +1669,8 @@ aot_create_func_contexts(const AOTCompData *comp_data, AOTCompContext *comp_ctx)
 }
 
 static bool
-aot_set_llvm_basic_types(AOTLLVMTypes *basic_types, LLVMContextRef context)
+aot_set_llvm_basic_types(AOTLLVMTypes *basic_types, LLVMContextRef context,
+                         int pointer_size)
 {
     basic_types->int1_type = LLVMInt1TypeInContext(context);
     basic_types->int8_type = LLVMInt8TypeInContext(context);
@@ -1768,9 +1735,19 @@ aot_set_llvm_basic_types(AOTLLVMTypes *basic_types, LLVMContextRef context)
     basic_types->funcref_type = LLVMInt32TypeInContext(context);
     basic_types->externref_type = LLVMInt32TypeInContext(context);
 
+    if (pointer_size == 4) {
+        basic_types->intptr_type = basic_types->int32_type;
+        basic_types->intptr_ptr_type = basic_types->int32_ptr_type;
+    }
+    else {
+        basic_types->intptr_type = basic_types->int64_type;
+        basic_types->intptr_ptr_type = basic_types->int64_ptr_type;
+    }
+
     return (basic_types->int8_ptr_type && basic_types->int8_pptr_type
             && basic_types->int16_ptr_type && basic_types->int32_ptr_type
-            && basic_types->int64_ptr_type && basic_types->float32_ptr_type
+            && basic_types->int64_ptr_type && basic_types->intptr_type
+            && basic_types->intptr_ptr_type && basic_types->float32_ptr_type
             && basic_types->float64_ptr_type && basic_types->i8x16_vec_type
             && basic_types->i16x8_vec_type && basic_types->i32x4_vec_type
             && basic_types->i64x2_vec_type && basic_types->f32x4_vec_type
@@ -2117,7 +2094,7 @@ jit_stack_size_callback(void *user_data, const char *name, size_t namelen,
         return;
     }
     /* ensure NUL termination */
-    bh_memcpy_s(buf, (uint32)sizeof(buf), name, (uint32)namelen);
+    bh_memcpy_s(buf, sizeof(buf), name, namelen);
     buf[namelen] = 0;
 
     ret = sscanf(buf, AOT_FUNC_INTERNAL_PREFIX "%" SCNu32, &func_idx);
@@ -2138,7 +2115,7 @@ jit_stack_size_callback(void *user_data, const char *name, size_t namelen,
 
     /* Note: -1 == AOT_NEG_ONE from aot_create_stack_sizes */
     bh_assert(comp_ctx->jit_stack_sizes[func_idx] == (uint32)-1);
-    comp_ctx->jit_stack_sizes[func_idx] = (uint32)stack_size + call_size;
+    comp_ctx->jit_stack_sizes[func_idx] = stack_size + call_size;
 }
 
 static bool
@@ -2346,6 +2323,9 @@ aot_create_comp_context(const AOTCompData *comp_data, aot_comp_option_t option)
 
     if (option->builtin_intrinsics)
         comp_ctx->builtin_intrinsics = option->builtin_intrinsics;
+
+    if (option->enable_gc)
+        comp_ctx->enable_gc = true;
 
     comp_ctx->opt_level = option->opt_level;
     comp_ctx->size_level = option->size_level;
@@ -2780,16 +2760,6 @@ aot_create_comp_context(const AOTCompData *comp_data, aot_comp_option_t option)
             aot_set_last_error("create LLVM target machine failed.");
             goto fail;
         }
-
-        /* If only to create target machine for querying information, early stop
-         */
-        if ((arch && !strcmp(arch, "help")) || (abi && !strcmp(abi, "help"))
-            || (cpu && !strcmp(cpu, "help"))
-            || (features && !strcmp(features, "+help"))) {
-            LOG_DEBUG(
-                "create LLVM target machine only for printing help info.");
-            goto fail;
-        }
     }
 
     triple = LLVMGetTargetMachineTriple(comp_ctx->target_machine);
@@ -2873,7 +2843,8 @@ aot_create_comp_context(const AOTCompData *comp_data, aot_comp_option_t option)
         goto fail;
     }
 
-    if (!aot_set_llvm_basic_types(&comp_ctx->basic_types, comp_ctx->context)) {
+    if (!aot_set_llvm_basic_types(&comp_ctx->basic_types, comp_ctx->context,
+                                  comp_ctx->pointer_size)) {
         aot_set_last_error("create LLVM basic types failed.");
         goto fail;
     }
@@ -2951,7 +2922,7 @@ aot_destroy_comp_context(AOTCompContext *comp_ctx)
         LLVMOrcDisposeLLLazyJIT(comp_ctx->orc_jit);
 
     if (comp_ctx->func_ctxes)
-        aot_destroy_func_contexts(comp_ctx, comp_ctx->func_ctxes,
+        aot_destroy_func_contexts(comp_ctx->func_ctxes,
                                   comp_ctx->func_ctx_count);
 
     if (bh_list_length(&comp_ctx->native_symbols) > 0) {
@@ -2966,10 +2937,6 @@ aot_destroy_comp_context(AOTCompContext *comp_ctx)
 
     if (comp_ctx->target_cpu) {
         wasm_runtime_free(comp_ctx->target_cpu);
-    }
-
-    if (comp_ctx->aot_frame) {
-        wasm_runtime_free(comp_ctx->aot_frame);
     }
 
     wasm_runtime_free(comp_ctx);
@@ -3050,8 +3017,7 @@ aot_get_native_symbol_index(AOTCompContext *comp_ctx, const char *symbol)
 }
 
 void
-aot_value_stack_push(const AOTCompContext *comp_ctx, AOTValueStack *stack,
-                     AOTValue *value)
+aot_value_stack_push(AOTValueStack *stack, AOTValue *value)
 {
     if (!stack->value_list_head)
         stack->value_list_head = stack->value_list_end = value;
@@ -3060,38 +3026,10 @@ aot_value_stack_push(const AOTCompContext *comp_ctx, AOTValueStack *stack,
         value->prev = stack->value_list_end;
         stack->value_list_end = value;
     }
-
-    if (comp_ctx->aot_frame) {
-        switch (value->type) {
-            case VALUE_TYPE_I32:
-            case VALUE_TYPE_I1:
-                push_i32(comp_ctx->aot_frame, value);
-                break;
-            case VALUE_TYPE_I64:
-                push_i64(comp_ctx->aot_frame, value);
-                break;
-            case VALUE_TYPE_F32:
-                push_f32(comp_ctx->aot_frame, value);
-                break;
-            case VALUE_TYPE_F64:
-                push_f64(comp_ctx->aot_frame, value);
-                break;
-            case VALUE_TYPE_V128:
-                push_v128(comp_ctx->aot_frame, value);
-                break;
-            case VALUE_TYPE_FUNCREF:
-            case VALUE_TYPE_EXTERNREF:
-                push_ref(comp_ctx->aot_frame, value);
-                break;
-            default:
-                bh_assert(0);
-                break;
-        }
-    }
 }
 
 AOTValue *
-aot_value_stack_pop(const AOTCompContext *comp_ctx, AOTValueStack *stack)
+aot_value_stack_pop(AOTValueStack *stack)
 {
     AOTValue *value = stack->value_list_end;
 
@@ -3105,43 +3043,11 @@ aot_value_stack_pop(const AOTCompContext *comp_ctx, AOTValueStack *stack)
         value->prev = NULL;
     }
 
-    if (comp_ctx->aot_frame) {
-        bh_assert(value);
-        bh_assert(value->value == (comp_ctx->aot_frame->sp - 1)->value);
-        bh_assert(value->type == (comp_ctx->aot_frame->sp - 1)->type);
-
-        switch (value->type) {
-            case VALUE_TYPE_I32:
-            case VALUE_TYPE_I1:
-                pop_i32(comp_ctx->aot_frame);
-                break;
-            case VALUE_TYPE_I64:
-                pop_i64(comp_ctx->aot_frame);
-                break;
-            case VALUE_TYPE_F32:
-                pop_f32(comp_ctx->aot_frame);
-                break;
-            case VALUE_TYPE_F64:
-                pop_f64(comp_ctx->aot_frame);
-                break;
-            case VALUE_TYPE_V128:
-                pop_v128(comp_ctx->aot_frame);
-                break;
-            case VALUE_TYPE_FUNCREF:
-            case VALUE_TYPE_EXTERNREF:
-                pop_ref(comp_ctx->aot_frame);
-                break;
-            default:
-                bh_assert(0);
-                break;
-        }
-    }
-
     return value;
 }
 
 void
-aot_value_stack_destroy(AOTCompContext *comp_ctx, AOTValueStack *stack)
+aot_value_stack_destroy(AOTValueStack *stack)
 {
     AOTValue *value = stack->value_list_head, *p;
 
@@ -3186,14 +3092,14 @@ aot_block_stack_pop(AOTBlockStack *stack)
 }
 
 void
-aot_block_stack_destroy(AOTCompContext *comp_ctx, AOTBlockStack *stack)
+aot_block_stack_destroy(AOTBlockStack *stack)
 {
     AOTBlock *block = stack->block_list_head, *p;
 
     while (block) {
         p = block->next;
-        aot_value_stack_destroy(comp_ctx, &block->value_stack);
-        aot_block_destroy(comp_ctx, block);
+        aot_value_stack_destroy(&block->value_stack);
+        aot_block_destroy(block);
         block = p;
     }
 
@@ -3202,9 +3108,9 @@ aot_block_stack_destroy(AOTCompContext *comp_ctx, AOTBlockStack *stack)
 }
 
 void
-aot_block_destroy(AOTCompContext *comp_ctx, AOTBlock *block)
+aot_block_destroy(AOTBlock *block)
 {
-    aot_value_stack_destroy(comp_ctx, &block->value_stack);
+    aot_value_stack_destroy(&block->value_stack);
     if (block->param_types)
         wasm_runtime_free(block->param_types);
     if (block->param_phis)
