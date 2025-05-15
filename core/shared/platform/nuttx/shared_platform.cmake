3cd3ca69cba099415a043cc9810048c7faacb018
xuxin19 <xuxin19@xiaomi.com>
2025-05-15 10:06:59 +0800

    cmake(bugfix):add wamr compile missing source
    interpreters/wamr/wamr/core/iwasm/common/wasm_memory.c:708: undefined reference to `os_mremap'
    
    Signed-off-by: xuxin19 <xuxin19@xiaomi.com>

--- core/shared/platform/nuttx/shared_platform.cmake
+++ core/shared/platform/nuttx/shared_platform.cmake
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 set(PLATFORM_SHARED_DIR ${CMAKE_CURRENT_LIST_DIR})
+set(PLATFORM_COMMON_DIR ${CMAKE_CURRENT_LIST_DIR}/../common)
 set(PLATFORM_COMMON_POSIX_DIR ${CMAKE_CURRENT_LIST_DIR}/../common/posix)
 add_definitions(-DBH_PLATFORM_NUTTX)
 
@@ -9,6 +10,7 @@ include_directories(${PLATFORM_SHARED_DIR})
 include_directories(${PLATFORM_SHARED_DIR}/../include)
 
 include(${PLATFORM_COMMON_POSIX_DIR}/platform_api_posix.cmake)
+include(${PLATFORM_COMMON_DIR}/memory/platform_api_memory.cmake)
 
 include(${SHARED_DIR}/utils/uncommon/shared_uncommon.cmake)
 
@@ -24,7 +26,8 @@ endif()
 
 set(PLATFORM_SHARED_SOURCE
     ${source_all} ${PLATFORM_COMMON_MATH_SOURCE}
-    ${PLATFORM_COMMON_POSIX_SOURCE} ${UNCOMMON_SHARED_SOURCE})
+    ${PLATFORM_COMMON_POSIX_SOURCE} ${UNCOMMON_SHARED_SOURCE}
+    ${PLATFORM_COMMON_MEMORY_SOURCE})
 # remove posix_memmap.c for NuttX
 list(REMOVE_ITEM PLATFORM_SHARED_SOURCE
      ${PLATFORM_COMMON_POSIX_DIR}/posix_memmap.c)
